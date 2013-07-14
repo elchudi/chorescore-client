63f75428d2ad4b5e6d6cde1cae832329c12a56e8
gustavog <chudijunk@gmail.com>
2013-07-14 16:23:42 +0200

    fixes location of angular-resource when angular is 1.1.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function ( grunt ) {
         'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
         'vendor/angular-ui-utils/modules/route/route.js',
-        'vendor/angular-resource/angular-resource.js'
+        'vendor/angular/angular-resource.js'
       ]
     },
 
